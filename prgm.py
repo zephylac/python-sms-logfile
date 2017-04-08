72d7d9d9851ff4bf1970b0f6a0c58ff658034e6d
zephylac <zephylac@users.noreply.github.com>
2017-04-08 13:24:04 +0200

    logfile filtering
    added exception for logfile of the day

--- prgm.py
+++ prgm.py
@@ -2,7 +2,7 @@ import os
 import re
 import sms
 import sqlite3
-
+import time
 
 conn = sqlite3.connect('log.db')
 conn.text_factory = str
@@ -22,18 +22,19 @@ for filename in os.listdir('.'):
             print filename
             # DATE FOR SMS
             date = re.findall(r'L(.*).log', filename)
-            #add logfile to treated list
-            cursor.execute('''INSERT INTO logfiles(name) VALUES (?)''',(filename,))
-            with open(filename) as origin_file:
-                for line in origin_file:
-                    #searching for kick
-                    if(re.findall(r'kicked|banned|slapped', line)) :
-                        test = re.sub('"','',line)
-                        test = test.partition(']')[2]
-                        test = re.sub('<.*?>|reason','',test)
-                        array.append(test)
-                        print test
-                print "\n\n"
+            if date[0] != (time.strftime("%Y%m%d")) :
+                #add logfile to treated list
+                cursor.execute('''INSERT INTO logfiles(name) VALUES (?)''',(filename,))
+                with open(filename) as origin_file:
+                    for line in origin_file:
+                        #searching for kick
+                        if(re.findall(r'kicked|banned|slapped', line)) :
+                            test = re.sub('"','',line)
+                            test = test.partition(']')[2]
+                            test = re.sub('<.*?>|reason','',test)
+                            array.append(test)
+                            print test
+                    print "\n\n"
 #sms.send(test)
 conn.commit()
 conn.close();
