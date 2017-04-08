f6ef1d381201fbae25c6260fc3e71b8fe9c54861
zephylac <zephylac@users.noreply.github.com>
2017-04-08 23:23:12 +0200

    Update prgm.py

--- prgm.py
+++ prgm.py
@@ -4,12 +4,27 @@ import sms
 import sqlite3
 import time
 
+class data:
+    """Class defining data :
+    - date of the logfile
+    - ban list
+    - kick list
+    - slap list
+    """
+
+
+    def __init__(self,date,ban,kick,slap):
+        self.date = date
+        self.ban  = ban
+        self.kick = kick
+        self.slap = slap
+
+        
 conn = sqlite3.connect('log.db')
 conn.text_factory = str
 cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS logfiles(name text)''')
-
 
 array = []
 for filename in os.listdir('.'):
@@ -19,12 +34,14 @@ for filename in os.listdir('.'):
         #test if logfile has been treated
         value = cursor.fetchone()
         if not value or value[0] != filename :
-            print filename
             # DATE FOR SMS
             date = re.findall(r'L(.*).log', filename)
             if date[0] != (time.strftime("%Y%m%d")) :
                 #add logfile to treated list
                 cursor.execute('''INSERT INTO logfiles(name) VALUES (?)''',(filename,))
+                ban = []
+                kick = []
+                slap = []
                 with open(filename) as origin_file:
                     for line in origin_file:
                         #searching for kick
@@ -32,9 +49,15 @@ for filename in os.listdir('.'):
                             test = re.sub('"','',line)
                             test = test.partition(']')[2]
                             test = re.sub('<.*?>|reason','',test)
-                            array.append(test)
-                            print test
-                    print "\n\n"
+                            if(re.findall(r'banned', test)) :
+                                ban.append(test)
+                            if(re.findall(r'kicked', test)) :
+                                kick.append(test)
+                            if(re.findall(r'slapped', test)) :
+                                slap.append(test)
+
+                array.append(data(date,ban,kick,slap))
+
 #sms.send(test)
 conn.commit()
-conn.close();
+conn.close();   
